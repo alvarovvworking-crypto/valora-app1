4eb548b483cb27bf151ded5c565b51194012099e
AlvaroVVia <villasenoralvaro0@gmail.com>
2026-06-18 13:49:30 +0200

    Fix build errors

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
-console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
-console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
